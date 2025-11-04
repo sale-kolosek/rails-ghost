94a257d54e0c2c60668e4ecb749fccf776bc0422
Aleksandar M. <151128252+sale-kolosek@users.noreply.github.com>
2025-11-04 13:51:16 +0100

    [#431227] Improve SEO (#70)
    * [#431227] Improve SEO
    
    * [#431227] Remove default alt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,17 @@ module ApplicationHelper
 		page_metadata = PageMetadata.find_by_slug(slug)
 		page_metadata.meta_title if page_metadata.present?
 	end
+
+	def fix_missing_img_alts(html, alt)
+	  doc = Nokogiri::HTML.fragment(html)
+
+	  doc.css('img').each do |img|
+	    if img['alt'].blank?
+	      img['alt'] = alt
+	    end
+	  end
+
+	  doc.to_html
+	end
+
 end
